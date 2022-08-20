8967189f28ec7ae22780e9c62360227ad22ed24d
Geisianny <geisiannybernardo@gmail.com>
2022-08-20 16:29:57 -0300

    criacao da pasta config, com as configuracoes do rails

---
+++ consultaSim/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :menu
+  resources :consulta
+  resources :medicos
+  resources :pacientes do
+    resources :enderecos
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
