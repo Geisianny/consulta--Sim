0f9d0820f84b6f54110257cb102c29ab4692d89f
Geisianny <geisiannybernardo@gmail.com>
2022-08-28 17:49:20 -0300

    Criando os testes do model Medico

--- consultaSim/test/models/medico_test.rb
+++ consultaSim/test/models/medico_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test 'Nao deve criar um cadastro de medico sem adicionar as informacoes basicas' do
+    medico = Medico.new
+    assert_not medico.save
+  end
+
+  test 'Deve criar um cadastro de medico corretamente' do
+    medico = Medico.new nomeCompleto: 'Geisianny teste', cpf: '12345678910', email: 'geisiannyteste@gmail.com',
+                        especialidade: 'neuro',crm:'12345'
+    assert medico.save
+  end
+
 end
