8ee416ea63d25adbc9c5ef060c5f0aaa4478653e
Geisianny <geisiannybernardo@gmail.com>
2022-08-28 22:04:00 -0300

    Criando testes do model Paciente

--- consultaSim/test/models/paciente_test.rb
+++ consultaSim/test/models/paciente_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  test "Nao deve salvar paciente sem o nome preenchido" do
+    paciente = Paciente.new nomeCompleto: '',dataDeNascimento: "28/08/2000",
+                            cpf:"12345678911", email: "pacientesemnome@gmail.com",
+                            endereco_attributes:{ cidade: 'Garanhuns', logradouro: "rua da padaria",
+                                                 complemento: 'apartamento', bairro: "Heliopes", cep: '12345678'}
+    assert_not paciente.save
+  end
+
+  test "Deve salvar paciente corretamente" do
+    paciente = Paciente.new nomeCompleto: 'Geisianny Paciente', dataDeNascimento:"28/08/2000",
+                            cpf:"22222222222", email: "GeisiannyPaciente@gmail.com",
+                    endereco_attributes:{cidade: 'Garanhuns', logradouro: "rua da padaria",
+                                         complemento: 'apartamento', bairro: "Heliopes", cep: '12345678'}
+    assert paciente.salve
+  end
+
+  end
