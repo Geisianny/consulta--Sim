cd2ad2edffc36327cbfafad17d57fe109f9911fe
Geisianny <geisiannybernardo@gmail.com>
2022-08-29 20:41:13 -0300

    Consertado as historia de usuario de consulta e criacao dos arquivos  .steps

---
+++ consultaSim/features/step_definitions/paciente_steps.rb
@@ -0,0 +1,34 @@
+Given('eu estou na pagina de cadastro de paciente') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When('eu crio um paciente com o nomeCompleto: {string}, dataDeNascimento: {string}, cpf:{string},email: {string}, cidade: {string}, logradouro: {string}, complemeto:{string}, bairro: {string}, cep: {string}') do |string, string2, string3, string4, string5, string6, string7, string8, string9|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When('eu seleciono criar paciente') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then('vejo uma mensagem que avisa que o paciente foi criado') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Given('pessoa com cpf: {string} existe no sistema') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When('eu clico em editar e edito o logradouro para {string}, do paciente com cpf: {string}') do |string, string2|
+  
+Given('pessoa com cpf: {string} existe no sistema') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When('eu clico em editar e edito o logradouro para {string}, do paciente com cpf: {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then('eu recebo uma mensagem que avisa que o paciente com cpf:{string} foi editado') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
